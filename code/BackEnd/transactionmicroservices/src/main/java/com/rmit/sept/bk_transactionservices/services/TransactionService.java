dfe83ef2f1e9050051f4f680bb2bde751a17b567
Reylan Gill <s3285137@student.rmit.edu.au>
2021-10-17 01:10:42 +1100

    implemented but not tested

---
+++ code/BackEnd/transactionmicroservices/src/main/java/com/rmit/sept/bk_transactionservices/services/TransactionService.java
@@ -0,0 +1,97 @@
+package com.rmit.sept.bk_transactionservices.services;
+
+
+import com.rmit.sept.bk_transactionservices.Repositories.ListingRepository;
+import com.rmit.sept.bk_transactionservices.Repositories.TransactionRepository;
+import com.rmit.sept.bk_transactionservices.exceptions.TransactionNotFoundException;
+import com.rmit.sept.bk_transactionservices.model.Listing;
+import com.rmit.sept.bk_transactionservices.model.Transaction;
+import com.rmit.sept.bk_transactionservices.payload.NoTransactionsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+    @Autowired
+    private ListingRepository listingRepository;
+
+
+    public Transaction getTransaction(Long id) {
+        Transaction transaction = transactionRepository.getById(id);
+        if (transaction == null) throw new TransactionNotFoundException("Transaction with id " + id + " not found");
+
+        updateCancellableIfTwoHoursElapsed(transaction);
+        return transaction;
+    }
+
+    public ResponseEntity<?> getTransactionsByUsername(String username) {
+        Map<String, List<Transaction>> transactions = new HashMap<String, List<Transaction>>();
+
+        List<Transaction> asSeller = transactionRepository.findBySeller(username);
+        if (!asSeller.isEmpty()) {
+            updateCancellableIfTwoHoursElapsed(asSeller);
+            transactions.put("asSeller", asSeller);
+        }
+        List<Transaction> asBuyer = transactionRepository.findByBuyer(username);
+        if (!asBuyer.isEmpty()) {
+            updateCancellableIfTwoHoursElapsed(asBuyer);
+            transactions.put("asBuyer", asBuyer);
+        }
+
+        if (transactions.isEmpty()) {
+            return ResponseEntity.ok(new NoTransactionsResponse("user '" + username));
+        }
+        return new ResponseEntity<Map<String, List<Transaction>>>(transactions, HttpStatus.OK);
+    }
+
+    public boolean cancelTransaction(Long id) {
+        Transaction transaction = getTransaction(id);
+        if (!transaction.getStatus().equals(Transaction.STATUS_CANCELLABLE)) {
+            return false;
+        }
+        transaction.setStatus(Transaction.STATUS_CANCELLED);
+
+        // allow listing to be shown again (no longer 'sold')
+        Listing listing = listingRepository.getById(transaction.getListingId());
+        if (listing != null) {
+            listing.setSold(false);
+        }
+
+        return true;
+    }
+
+    private void updateCancellableIfTwoHoursElapsed(List<Transaction> transactions) {
+        long now = new Date().getTime();
+        long twoHourDuration = TimeUnit.HOURS.toMillis(2);
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getStatus().equals(Transaction.STATUS_CANCELLABLE)) {
+                if ((transaction.getDate().getTime() + twoHourDuration) < now) {
+                    transaction.setStatus(Transaction.STATUS_CONFIRMED);
+                }
+            }
+        }
+    }
+
+    private void updateCancellableIfTwoHoursElapsed(Transaction transaction) {
+        long now = new Date().getTime();
+        long twoHourDuration = TimeUnit.HOURS.toMillis(2);
+
+        if (transaction.getStatus().equals(Transaction.STATUS_CANCELLABLE)) {
+            if ((transaction.getDate().getTime() + twoHourDuration) < now) {
+                transaction.setStatus(Transaction.STATUS_CONFIRMED);
+            }
+        }
+    }
+}
